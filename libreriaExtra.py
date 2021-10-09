b8227fbfa7394f764328058ac2d6f15030e02d30
ggv8 <gabriel.go.v8@gmail.com>
2021-10-08 19:01:14 -0600

    Ultima edición actualizada

--- libreriaExtra.py
+++ libreriaExtra.py
@@ -1,6 +1,6 @@
 # Elaborador por: Ignacio García Galagarza, Gabriel Gomez Vega
 # Fecha de creación: 21/09/2021, 12:14 AM
-# Última edición: 21/09/2021, XX:XX AM
+# Última edición: 08/10/2021, 06:00 AM
 # Versión: 3.9.6
 
 ##############################################################
